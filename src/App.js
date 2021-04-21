2db8bac0a946ffb12028b936d9b4b19af2fd9ea1
soundsXsweet <57301173+soundsXsweet@users.noreply.github.com>
2021-04-20 20:20:15 -0500

    react-example removing comments, would rather utilize this project as a live teaching tool

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-//The import { Target, OtherTarget } syntax was introduced with ES6 
 import { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import ParentComponent from './components/ParentComponent.js';
 
-//Another ES6 feature, class declarations
-//When you use create-react-app, your components may be initialized as 'functional' components
-//You can utilize these as-is, or convert them into class components by adding the necessary syntax.
-//Personally, I opt for the latter.
 class App extends Component {
-
-
-//The render method is simply a return statement that contains React elements.
-//This allows us to return the needed elements to enable our DOM to display what we want.
-//Because 'class' is a reserved keyword in React, we use className to mark JSX elements with css classes
   render() {
     return(
       <>
